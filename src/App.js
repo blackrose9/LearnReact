9fdc652194988da3231ea17051a1afaddff107a2
blackrose9 <sarah.mwendwa@outlook.com>
2019-06-24 14:32:38 +0200

    Individual Exercise
    Added a form for user to input their to-do's
    Added a list to display all the to-do's
    
    //duplicated the code in App.js (as a start) to test if it is displaying correctly.
    Conclusion: it is not displaying anything at all for now.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  
-}
+class App extends React.Component{
+    //used Babel to convert my HTML into JSX. for learning purposes.
+      render(){
+          return(
+    //creates the section which will hold the form and the list
+    React.createElement("section", null, 
+    //this is the beginning of the form element
+    React.createElement("form", {
+      id: "todo"
+    }, 
+    //the code below creates a user input text box and a button to add their input to the list. (no functionality yet)
+    React.createElement("input", {
+      type: "text",
+      placeholder: "To Do"
+    }, " "), 
+    React.createElement("input", {
+      type: "button",
+      onclick: "addToDo"
+    }, "Add")), //end of our form tag 
 
+    //this div element encloses our list
+    React.createElement("div", {
+      id: "list"
+    }, 
+    //the code below creates our main list with it's own checkbox
+    React.createElement("ul", null, 
+
+    React.createElement("li", null, 
+    React.createElement("input", {
+      type: "checkbox"
+    }, " "), 
+    React.createElement("label", null, "I am a list item!"), 
+    //the code below creates our sublist complete with it's own checkbox
+    React.createElement("ul", {
+      id: "sublist"
+    }, 
+    React.createElement("li", null, 
+    React.createElement("input", {
+      type: "checkbox"
+    }, " "), 
+    React.createElement("label", null, " I am a sublist item! ") 
+    ) //end of sub-list <li>
+    ) //end of sub-list <ul>
+    ) //end of main <li>
+    ) //end of main <ul>
+    ) //end of div id=list
+    ) //end of the section tag
+    );
+    }
+}
 export default App;
